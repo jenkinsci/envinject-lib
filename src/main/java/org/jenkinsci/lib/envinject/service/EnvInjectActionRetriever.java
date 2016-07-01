86088e7f6bc8fc702b4c61c95019d4136982ffc8
Félix Belzunce Arcos <fbelzunc@gmail.com>
2016-07-01 13:16:13 +0200

    Address comments

--- src/main/java/org/jenkinsci/lib/envinject/service/EnvInjectActionRetriever.java
+++ src/main/java/org/jenkinsci/lib/envinject/service/EnvInjectActionRetriever.java
@@ -19,12 +19,12 @@ public class EnvInjectActionRetriever {
     //with EnvInjectAction subclasses. Subclasses cannot be casted from
     //all point of Jenkins (classes are not loaded in some points)
     public Action getEnvInjectAction(AbstractBuild<?, ?> build) {
-        List<Action> actions;
         if (build == null) {
             throw new NullPointerException("A build object must be set.");
         }
         try {
             Class<?> matrixClass = Class.forName("hudson.matrix.MatrixRun");
+            List<Action> actions;
             if (matrixClass.isInstance(build)) {
                 Method method = matrixClass.getMethod("getParentBuild", null);
                 Object object = method.invoke(build);
@@ -32,26 +32,24 @@ public class EnvInjectActionRetriever {
                     build = (AbstractBuild<?, ?>) object;
                 }
             }
+            actions = build.getActions();
+            for (Action action : actions) {
+                if (action == null) {
+                    continue;
+                }
+
+                if (EnvInjectAction.URL_NAME.equals(action.getUrlName())) {
+                    return action;
+                }
+            }
         } catch (ClassNotFoundException e) {
-            LOGGER.log(Level.FINEST, String.format("hudson.matrix.MatrixRun is missed", e));
+            LOGGER.log(Level.FINEST, String.format("hudson.matrix.MatrixRun is not installed", e));
         } catch (NoSuchMethodException e) {
             LOGGER.log(Level.WARNING, String.format("The method getParentBuild does not exist for hudson.matrix.MatrixRun", e));
         } catch (IllegalAccessException e) {
             LOGGER.log(Level.WARNING, String.format("There was a problem in the invocation of getParentBuild in hudson.matrix.MatrixRun", e));
         } catch (InvocationTargetException e) {
             LOGGER.log(Level.WARNING, String.format("There was a problem in the invocation of getParentBuild in hudson.matrix.MatrixRun", e));
-        } finally {
-            actions = build.getActions();
-        }
-
-        for (Action action : actions) {
-            if (action == null) {
-                continue;
-            }
-
-            if (EnvInjectAction.URL_NAME.equals(action.getUrlName())) {
-                return action;
-            }
         }
         return null;
     }
