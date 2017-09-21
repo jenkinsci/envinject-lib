af6ca5522ee47db6ac0c9b73f6205b3e759cf78b
Devin Nusbaum <d.w.nusbaum@gmail.com>
2017-09-21 09:20:20 -0400

    Revert "Set envMap after the build is set in onLoad"
    This reverts commit faa98738e8355d80d52e796d0d2dcff03e71318e.

--- src/main/java/org/jenkinsci/lib/envinject/EnvInjectAction.java
+++ src/main/java/org/jenkinsci/lib/envinject/EnvInjectAction.java
@@ -45,9 +45,6 @@ public class EnvInjectAction implements RunAction2, StaplerProxy {
     @Override
     public void onLoad(Run<?, ?> run) {
         build = run;
-        if (envMap == null) {
-            envMap = getEnvMap();
-        }
     }
  
     /**
