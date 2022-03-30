d0862e87fe86689ceb40dcecad58532bb594972f
NotMyFault <mc.cache@web.de>
2022-03-30 21:11:43 +0200

    build: Utilize bom

---
+++ src/test/java/org/jenkinsci/lib/envinject/DummyTest.java
@@ -0,0 +1,13 @@
+package org.jenkinsci.lib.envinject;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class DummyTest {
+
+    @Test
+    public void dummyTrue() {
+        assertTrue(true);
+    }
+}
